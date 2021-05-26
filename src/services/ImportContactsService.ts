e5c0e5344f38b0b6498403ed9046f82fc54d65b1
Johnny <jcarreiropublicidade@gmail.com>
2021-05-25 21:17:58 -0300

    feat: Create Import Service

--- src/services/ImportContactsService.ts
+++ src/services/ImportContactsService.ts
@@ -1,5 +1,28 @@
 import { Readable } from 'stream'
+import csvParse from 'csv-parse'
+
+import Contact from '@schemas/Contact'
+import Tag from '@schemas/Tag'
 
 export class ImportContactsService {
-  async run(contactFileStrem: Readable): Promise<void> {}
+  async run(contactsFileStrem: Readable, tags: string[]): Promise<void> {
+    const parsers = csvParse({
+      delimiter: ';'
+    })
+    const parseCsv = contactsFileStrem.pipe(parsers)
+
+    const tagsData = tags.map(tag => ({
+      title: tag
+    }))
+    const createdTags = await Tag.create(tagsData)
+    const tagIds = createdTags.map(tag => tag._id)
+
+    parseCsv.on('data', async line => {
+      const [email] = line
+
+      await Contact.create({ email, tags: tagIds })
+    })
+
+    await new Promise(resolve => parseCsv.on('end', resolve))
+  }
 }
